207da9b4dd32fc47030b8d9d9c087bb20d6c48a1
Ana Grifoni <ana.grifoni@aluno.senai.br>
2026-03-05 07:47:39 -0300

    add: fazendo o resto

--- js/global.js
+++ js/global.js
@@ -1,7 +1,40 @@
-const btnP1 = document.querySelector('#inputP1');
-const btnP2 = document.querySelector('#inputP2');
-const btnP3 = document.querySelector('#inputP3');
+//1. Logica do menu principal (index, obter os elementos)
+const buttonP1 = document.querySelector('#inputP1');
+const buttonP2 = document.querySelector('#inputP2');
+const buttonP3 = document.querySelector('#inputP3');
 
-const verificaBotao = () => {
-    if(btnP1)
-};
+//2. metodo addEventListener com uma função anonima 
+
+buttonP1.addEventListener('click', function(){
+    window.location.href='./pages/projeto01.html'
+});
+buttonP2.addEventListener('click', function(){
+    window.location.href='./pages/projeto02.html'
+});
+buttonP3.addEventListener('click', function(){
+    window.location.href='./pages/projeto03.html'
+});
+
+function criarBotaoVoltar (){
+    //1. criar o elemento botao
+    const botaovoltar = document.createElement('button');
+
+    //2. definir o estilo do botao 
+    botaovoltar.innerText ='voltar';
+    botaovoltar.style.marginTop = '20px'
+    function navegar(){
+        window.location.href = '../index.html'
+    }
+
+    botaovoltar.addEventListener('click', navegar)
+
+    // //3. lógica o click 
+    // botaovoltar.addEventListener('click', function(){
+    //     window.location.href = "../index.html"
+    // })
+
+    //4. adicionar o botao no final do body (appendchild, adiionar o filho (elemento, ou seja, o corpo do botao))
+    document.body.appendChild(botaovoltar)
+;}
+
+window.onload = criarBotaoVoltar();
